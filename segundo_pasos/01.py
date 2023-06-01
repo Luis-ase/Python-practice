ecdac78f58e9a0748157f80147fb97ad01c6de41
Michael Luis Condori Paye <luis2003nb@gmail.com>
2023-06-01 10:00:19 -0300

    Practic Python 08

--- segundo_pasos/01.py
+++ segundo_pasos/01.py
@@ -2,12 +2,23 @@
 Vimos anteriormente el programacion estructurado 
 
 """
+def convertir_a_año(f):
+    return f //10000
+
+def convertir_a_mes(f:int):
+    return (f//100) %100
+
+def convertir_a_dia(f:int):
+    return f%100
+
+
 def main():
     numero = 19851217
-    año = numero//10000
-    dia = numero%100
-    mes = (numero//100) %100
-    print(f"fecha : {numero}  {año}-{mes}-{dia}") 
+    año = convertir_a_año(numero)
+    dia = convertir_a_dia(numero)
+    mes = convertir_a_mes(numero)
+    print(f"fecha : {numero}  {año}-{mes}-{dia}")
+
 
 
 main()
